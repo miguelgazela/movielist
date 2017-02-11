d5bf60d4cd8889bb21bf4094a70579eb7888a7c7
Miguel Oliveira  <miguel.oliveira@farfetch.com>
2017-02-11 18:30:02 +0000

    Added automatic counters

--- js/main.js
+++ js/main.js
@@ -6,4 +6,12 @@ $(document).ready(function() {
     columnWidth: 260
   });
 
+  $('.pagetitle').each(function(index, element){
+
+    var counter = $(this).next().children().length;
+
+    var $counter = $($(this).children('.movie-counter')[0]);
+    $counter.html(counter);
+  });
+
 });
