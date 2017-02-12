a7048c978edb8ad768bc60ec4b2f4bbd8bc0336b
Miguel Oliveira  <miguel.oliveira@farfetch.com>
2017-02-12 11:26:46 +0000

    Added template engine to build the movie lists

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 $(document).ready(function() {
 
-  $('.movielist').masonry({
-    // options
-    itemSelector: '.movieitem',
-    columnWidth: 260
+  // $('.pagetitle').each(function(index, element){
+  //
+  //   var counter = $(this).next().children().length;
+  //
+  //   var $counter = $($(this).children('.movie-counter')[0]);
+  //   $counter.html(counter);
+  // });
+
+  $.getJSON("movies.json", function(json) {
+
+    var yearListTmpl = $.templates("#yearListTemplate");
+    var $yearsList = $('#years');
+
+    var queue = json.queue;
+
+    var queueInfo = {year: "queue", movies: queue, numMovies: queue.length};
+    $yearsList.append(yearListTmpl.render(queueInfo));
+
+    $.each(json.years, function(index, yearObj) {
+
+      var yearInfo = {year: yearObj.year, movies: yearObj.movies, numMovies: yearObj.movies.length};
+
+      $yearsList.append(yearListTmpl.render(yearInfo));
+    });
+
+    $('.movielist').masonry({
+      // options
+      itemSelector: '.movieitem',
+      columnWidth: 260
+    });
+
   });
-
-  $('.pagetitle').each(function(index, element){
-
-    var counter = $(this).next().children().length;
-
-    var $counter = $($(this).children('.movie-counter')[0]);
-    $counter.html(counter);
-  });
-
 });
